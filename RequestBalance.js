02776a86bf435371eba85f022cc41916df5f2446
Jon Mark Perry <jonmarkperry@gmail.com>
2015-04-09 13:08:38 +0100

    Update RequestBalance.js

--- RequestBalance.js
+++ RequestBalance.js
@@ -1,4 +1,4 @@
 function RequestBalance(sortCode, accountNumber, authCode, keyCode) {
-if (authorize(authCode,keyCode)) return sortCode.accountNumber.balance;
+if (Authorize(authCode,keyCode)) return sortCode.accountNumber.balance;
 return 'request unauthorized';
 }
